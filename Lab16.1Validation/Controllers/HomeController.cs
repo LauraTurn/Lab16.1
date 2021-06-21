f1cbe01d6be01afa407be54efdbd2d5464c04d0a
Laura Turner <lauraturner@quickenloans.com>
2021-06-21 19:57:42 -0400

    second time

---
+++ Lab16.1Validation/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Lab16._1Validation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab16._1Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult RegForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UserInfo(NUser newuser)
+        {
+            NUser.FullInfo.Add(newuser);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lab16.1Validation/Models/NUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab16._1Validation.Models
+{
+    public class NUser
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string confirmpassword { get; set; }
+
+        public static List<NUser> FullInfo = new List<NUser>();
+    }
+
+}
